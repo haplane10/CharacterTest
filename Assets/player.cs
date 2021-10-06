184fe38ef559c015b8dde1284af4ae5356a5f379
JungHo Park <haplane5@gmail.com>
2021-10-06 12:31:25 +0900

    업데이트

---
+++ Assets/player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class player : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    
+}
